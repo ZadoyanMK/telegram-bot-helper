5baa52a8288ce6bc2a01527513eb51e00075b35a
ZadoyanMK <konzamir@gmail.com>
2019-05-23 02:39:32 +0300

    update telegram bot

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from telegram.ext import Updater, CommandHandler
-from telegram import Contact
+import telegram
 import requests
-import re
+from lxml import html
+import random
+import logging
 
 
 def get_url():
@@ -10,20 +12,36 @@ def get_url():
     return url
 
 
+def get_second_url():
+    page = requests.get('https://www.pornpics.com/pornstar/')
+    tree = html.fromstring(page.content)
+    urls = tree.xpath('//a[contains(@class, "rel-link")]/img/@src')
+    url = random.choice(urls)
+    return url
+
+
 def bop(bot, update):
-    url = get_url()
     chat_id = update.message.chat_id
-    print('*' * 10)
-    print(chat_id)
-    print('*' * 10)
+    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
+    url = get_url()
     bot.send_photo(chat_id=chat_id, photo=url)
     # bot.send_message(chat_id=chat_id, text="test")
 
 
+def t(bot, update):
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
+    url = get_second_url()
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+
 def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     updater = Updater('859269592:AAFCO36kDjNP_LNHAq9xFlb4O_4rkY-B-U0')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop', bop))
+    dp.add_handler(CommandHandler('t', t))
     updater.start_polling()
     updater.idle()
 
